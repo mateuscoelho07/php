14dc06eee0ae53db00b459ffb96760510d918129
mateuscoelho07 <mateuscoelhosola@gmail.com>
2023-08-10 13:37:53 -0300

    PHPAula1
    Aula 1 - Sintaxe e servidor local com XAMPP

---
+++ aula01/sintaxe1.php
@@ -0,0 +1,9 @@
+<?php
+    // Geração de texto (string)
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estruturado (com tags, atributos)*/
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP,</h2>";
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP,</h2>";
+
+?>    
