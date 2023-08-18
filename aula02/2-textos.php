88d252907f462186632deb4d910b3b4c67fc8a8f
mateuscoelho07 <mateuscoelhosola@gmail.com>
2023-08-18 10:11:15 -0300

    .

---
+++ aula02/2-textos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Texto (string) no PHP</title>
+</head>
+<body>
+    <h1>Tipos de dados texto no PHP</h1>
+    <p>Uma string é uma série de caracteres, onde um caractere é o mesmo que um byte.</p>
+    <ul>
+        <li>aspas simples</li>
+        <li>aspas duplas</li>
+    </ul>
+    
+    <!-- Código PHP para tipos de string -->
+    <?php
+
+        echo "Testando texto de aspas duplas <br>";
+        echo 'Testando texto de aspas simples <br>';
+        echo "<br><<p>Para exibir uma aspas, inicie a string com a outra.</p>";
+        echo "Ele disse: 'Olá' <br>";
+        echo 'Ele disse: "Olá" <br>';
+    
+        echo "<br><p>O valor é passado para a variável e depois recuperado na string.</p>";
+        
+
+
+
+
+
+
+
+
+
+
+    <br><hr>
+    <h2>Função de verificação</h2>
+    <p>Para saber se uma variável é string usamos a função is_string()</p>
+
+    <!-- Código PHP para verificar strings -->
+   
+</body>
+</html>
