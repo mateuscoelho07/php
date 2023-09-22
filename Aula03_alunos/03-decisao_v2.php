90ab54080a8cfae48f934e8ac23b85c49562aa88
mateuscoelho07 <mateuscoelhosola@gmail.com>
2023-09-22 15:24:17 -0300

    .

---
+++ Aula03_alunos/03-decisao_v2.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Condicionais PHP</title>
+</head>
+<body>
+    <h1 class="destaqueP">Estruturas de Controle Condicional</h1>
+    <hr>
+<!-- ___________________________________________________________________ -->
+    <h2 class="destaque">SE Simples</h2>
+
+
+<!-- 1ª Digitação (Aqui) -->
+    <?php
+        $numero = 5;
+        if ($numero >1) {
+            ?>
+            
+            <p><?=$numero?> é maior que 1 </p>
+        <?php
+ }
+?>
+
+
+<hr>
+<!-- ___________________________________________________________________ -->
+    <h2 class="destaque">SE Composta</h2>
+
+<?php
+    $produto = "Iphone 15 Pro Max";
+    $qtdEmEstoque = 0; 
+    $qtdCritica = 14; 
+?>
+
+    <h3><?=$produto?></h3>
+
+
+<!-- 2ª Digitação (Aqui) -->
+<?php
+        if($qtdEmEstoque < $qtdCritica) {
+    ?>
+        <p class="repor">É necessário comprar</p>
+    <?php
+        if($qtdEmEstoque === 0) {
+    ?>
+        <p><mark><b>Urgente</b></mark></p>
+    <?php
+        }
+    }
+        else {
+    ?>
+        <p class="ok">EStoque OK!</p>
+    <?php
+        }
+    ?>
+
+
+<hr>
+<!-- ___________________________________________________________________ -->
+    <h2 class="destaque">Encadeada</h2>
+
+     <!-- Cardápio digital
+    1 -> Pastel
+    2 -> Pizza
+    3 -> Esfiha
+    x -> Opção inválida  -->
+<?php
+    $opcao = 3; 
+
+    if($opcao === 1) { 
+?>
+        <p>Pastel</p>
+<?php
+    } elseif($opcao === 2) {
+?>
+        <p>Pizza</p>
+<?php
+    } elseif($opcao === 3) {
+?>
+        <p>Esfiha</p>
+<?php
+    } else {
+?>
+        <p>Opção inválida!</p>
+<?php
+    } 
+/*___________________________________________________________________ */
+    // Refatorada (Melhorada)
+    $opcao = 1; // comece com qualquer valor
+
+
+    //  3ª Digitação (Aqui) 
+
+    if($opcao ===1) {
+        $opcaoEscolhida = "Pastel";
+    } elseif($opcao ===2) {
+        $opcaoEscolhida = "Pizza";
+    } elseif($opcao ===3) {
+        $opcaoEscolhida ="Esfiha";
+    } else {
+        $opcaoEscolhida ="Opção inválida";
+    }
+
+?>
+    <p><?=$opcaoEscolhida?></p>
+<hr>
+
+<!-- ___________________________________________________________________ -->
+<h2 class="destaque">Switch/Case</h2>
+
+<!-- 4ª Digitação (Aqui) -->
+    
+<?php
+    switch($opcao) {
+        case 1: $pedido = "Pastel"; break;
+        case 2: $pedido = "Pizza"; break;
+        case 3: $pedido = "Esfiha"; break;
+        default: $pedido = "Opção inválida"; break;
+    }
+?>
+
+    <p><?=$pedido?></p>
+
+</body>
+</html>
---
+++ Aula03_alunos/04-loops.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>Loops PHP</title>
+</head>
+<body>
+    <h1 class="destaqueP">Estruturas de Controle e Repetição (Loops)</h1>
+    <hr>
+    <h2 class="destaque">While (enquanto)</h2>
+
+
+    <!-- 1ª Digitação (Aqui) -->
+
+    <?php
+        $i = 1;
+        while ($i <=5) {
+    ?>
+        <p><?=$i?></p>/
+    <?php
+        $i ++; //Incremento
+        }
+    ?>
+<hr>
+<!-- _______________________________________________________________________________ -->
+
+    <h2 class="destaque">do/while (repita)</h2>
+
+
+<!-- 2ª Digitação (Aqui) -->
+
+<?php
+    $j = 1;
+        do {
+?>
+
+<div><h3>Olá!</h3></div>
+
+<?php
+    $j++;
+} while ($j <=3);
+?>
+
+
+<?php
+
+?>
+
+
+
+
+<hr>
+<!-- _______________________________________________________________________________ -->
+    <h2 class="destaque">For</h2>
+
+    // Laço For
+    // para i de 1 até 10 faça
+
+    //Controle; Condição; Atualização
+
+    //  3ª Digitação (Aqui)
+
+    <?php
+    // Laço For
+    // para i de 1 até 10 faça
+
+    //Controle; Condição; Atualização
+
+for($i = 1; $i <=10; $i++) {
+    echo $i. "";
+}
+
+
+
+
+
+ ?>
+ <!-- _______________________________________________________________________________ -->
+
+ <!-- Exercício
+      1) Crie um array com os meses do ano
+      2) Usando um loop for, acesse e mostre os nomes dos meses dentro de uma lista ordenada
+ -->
+<hr>
+    <h2 class="destaque">Exercício meses</h2>
+
+<?php
+
+    $meses = array("Janeiro", "Fevereiro", "Março", "Abril","Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro","Novembro", "Dezembro");
+?>
+    <ol>
+
+
+        <!-- 4ª Digitação (Aqui) -->
+
+        <?php
+            $quantidade = count($meses);
+
+            for($k = 0; $k <$quantidade; $k++) {
+        ?>
+            <li><?=$meses[$k]?></li>
+        
+        <?php
+            }
+        ?>
+
+    </ol>
+
+<!-- _______________________________________________________________________________ -->
+<hr>
+    <h2 class="destaque">foreach (para cada)</h2>
+
+<ol>
+
+
+    <!-- 5ª Digitação (Aqui) -->
+    
+        <?php
+        foreach($meses as $mes) {
+?>
+        <li><?=$mes?></li>
+      <?php
+      }
+?>
+
+
+
+</ol>
+<!-- _______________________________________________________________________________ -->
+<hr>
+    <h2 class="destaque">Array associativo</h2>
+<?php
+    $clubes = [
+        "Corinthians" => "Timão",
+        "Palmeiras" => "Porco",
+        "São Paulo" => "Tricolor",
+        "Santos" => "Peixe",
+
+    ];
+
+    //  6ª Digitação (Aqui) 
+
+    foreach($clubes as $blube =>$apelido){
+        ?>
+        <p>
+            O <?=$clubes?> é conhecido como: <?=$apelido?>
+        </p>
+     <?php
+          }
+     ?>
+
+<!-- _______________________________________________________________________________ -->
+<hr>
+    <h2 class="destaque">Matriz (Associativa)</h2>
+<?php
+
+$alunos = [
+
+              [
+                  "nome" => "Chaves",
+                  "idade" => 8
+
+              ],
+              [
+                  "nome" => "Chapolin",
+                  "idade" => 20
+
+              ],
+              [
+                  "nome" => "Chiquinha",
+                  "idade" => 10
+
+              ]
+          ];
+
+    foreach($alunos as $aluno){
+?>
+
+  <!-- 7ª Digitação (Aqui) -->
+
+    <p>Nome: <?=$aluno['nome']?></p>
+    <p>Idade: <?=$aluno['idade']?></p>
+    <hr>
+
+
+<?php
+    }
+?>
+
+</body>
+</html>
